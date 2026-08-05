8e42d3b7579371f03edf3899e5d1cc0fe0fa236c
Divyanshu Singh <divyanshu150798@gmail.com>
2026-08-05 09:06:06 +0530

    build error fixes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,8 +3,13 @@ import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 
 const prismaClientSingleton = () => {
-  const connectionString = process.env.DATABASE_URL
+  const connectionString = process.env.DATABASE_URL || process.env.POSTGRES_URL;
+  if (!connectionString) {
+    console.warn("⚠️ WARNING: DATABASE_URL (or POSTGRES_URL) is not set in the environment variables!");
+  }
   const pool = new Pool({ connectionString })
+
+
   const adapter = new PrismaPg(pool)
   return new PrismaClient({ adapter })
 }
