1b6f384106cd13c9e908152a5fd94175495ed820
Divyanshu Singh <divyanshu150798@gmail.com>
2026-08-09 16:56:54 +0530

    hero image changed

--- src/components/figma-home/FigmaHero.tsx
+++ src/components/figma-home/FigmaHero.tsx
@@ -9,7 +9,7 @@ export function FigmaHero() {
       <div className="absolute inset-0 z-0">
         {/* Desktop Image */}
         <Image
-          src="/images/new/hero-new.png"
+          src="/images/new/hero-desktop.png"
           alt="Bamboo Bath Kit"
           fill
           className="object-cover object-center hidden lg:block"
